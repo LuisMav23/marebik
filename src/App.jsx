92e36ab5a1c55c76f1750c1dc99b7d9bbd024b2e
Mav023 <gabrielluismaverick@gmail.com>
2023-08-02 13:03:12 +0800

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <>
+      <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+        <div className="justify-content-center container-fluid">
+          <img/>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default App;
